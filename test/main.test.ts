2dda803d87760e4c69f5e0381c3df99b030dd9b4
stephane <kamga.stephane@gmail.com>
2022-01-04 22:01:15 +0100

    test:add test

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,7 @@
 import {CinetPay} from "../src";
 
 test('Create Cinet', () => {
-  const cinet = CinetPay.init({baseUrl: '', site_id: '', apikey:''});
+  const cinet = CinetPay.init({baseUrl: 'url', site_id: 'site_ir', apikey:'apikey'});
   expect(cinet).toBeDefined();
 });
 
